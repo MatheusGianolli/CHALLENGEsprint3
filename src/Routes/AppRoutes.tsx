8596130648982b65f86e9c8a0afc33cbb95d002e
Matheus Gianolli <matheusgianolli@hotmail.com>
2025-09-29 20:22:14 -0300

    feat: Adicinando heander

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -5,20 +5,27 @@ import AboutPage from '../pages/AboutPage';
 import ContactPage from '../pages/ContactPage';
 import FaqPage from '../pages/FaqPage';
 import TeamPage from '../pages/TeamPage';
-import ServiceDetailPage from '../pages/ServiceDetailPage'; // Rota dinâmica!
+import ServiceDetailPage from '../pages/ServiceDetailPage';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sobre" element={<AboutPage />} />
-        <Route path="/contato" element={<ContactPage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/integrantes" element={<TeamPage />} />
-        {/* Rota Dinâmica: Atende ao critério de useParams() */}
-        <Route path="/servico/:id" element={<ServiceDetailPage />} /> 
-      </Routes>
+      {/* Header e Footer ficam fora do <Routes> para aparecerem em todas as páginas */}
+      <Header />
+      <main className="pt-20"> {/* pt-20 para não ficar embaixo do header fixo */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sobre" element={<AboutPage />} />
+          <Route path="/contato" element={<ContactPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="/integrantes" element={<TeamPage />} />
+          {/* Rota Dinâmica para atender ao critério de useParams() */}
+          <Route path="/servico/:id" element={<ServiceDetailPage />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
